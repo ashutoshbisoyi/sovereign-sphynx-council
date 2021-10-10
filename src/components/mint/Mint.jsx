fd6e3a7e1e55d6459ef1d0b46f6952cf7a7df471
ashutoshbisoyi <ashutoshbisoyi205@gmail.com>
2021-10-10 19:13:15 +0530

    wallet

--- src/components/mint/Mint.jsx
+++ src/components/mint/Mint.jsx
@@ -4,75 +4,35 @@ import plus from '../../assets/plus.svg';
 import minus from '../../assets/minus.svg';
 import StarBg from '../star-bg/StarBg';
 import { ToastContainer, toast } from 'react-toastify';
-import metamask from '../../assets/metamask.png';
-import trustwallet from '../../assets/trustwallet.png';
-import cancel from '../../assets/cancel.png';
+
 const Mint = () => {
   const [count, setCount] = useState(1);
   const [verified, setVerified] = useState(false);
-  const [openPopup, setOpenPopup] = useState(false);
   //verify white list function
   const verifyWhiteList = () => {
     if (!verified) {
-      setOpenPopup(true);
+      handleSuccessVerify();
     }
-  };
-  const closePopUp = () => {
-    setOpenPopup(false);
   };
 
   const handleSuccessVerify = () => {
-    setOpenPopup(false);
-    toast('You are Whitelisted');
+    toast.success('You are Whitelisted');
     setVerified(true);
     setCount(20);
   };
-  const handleFailedVerify = () => {
-    setOpenPopup(false);
-    toast.error('You are not Whitelisted');
-    setVerified(false);
-  };
+
+  //to use if varification failed
+  // const handleFailedVerify = () => {
+  //   toast.error('You are not Whitelisted');
+  //   setVerified(false);
+  // };
 
   return (
     <>
       <section className='container-fluid mint' id='mint'>
         <ToastContainer />
         <StarBg />
-        {openPopup && (
-          <div className='modal-container'>
-            <div className='custom-modal gradient-container'>
-              <div className='content'>
-                <img
-                  src={cancel}
-                  alt='cancel'
-                  className='cancel'
-                  onClick={closePopUp}
-                />
-                <h4>Connect Wallet</h4>
-                <button onClick={handleSuccessVerify}>
-                  {' '}
-                  <img
-                    src={metamask}
-                    alt='meta mask'
-                    className='img-fluid me-3'
-                  />{' '}
-                  METAMASK
-                </button>
-                <button onClick={handleFailedVerify}>
-                  <img
-                    src={trustwallet}
-                    alt='meta mask'
-                    className='img-fluid me-3'
-                  />
-                  TRUSTWALLET
-                </button>
-                <div className='d-flex justify-content-center mt-5'>
-                  <a href='https://www.google.co.in/'>Learn how to connect?</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        )}
+
         <div className='container'>
           <h2>Mint a Sphynx</h2>
           <div className='counter-container'>
--- src/components/navigation-bar/NavigationBar.jsx
+++ src/components/navigation-bar/NavigationBar.jsx
@@ -1,84 +1,132 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './NavigationBar.scss';
 import logo from '../../assets/logo.png';
 import { Link } from 'react-scroll';
+import metamask from '../../assets/metamask.png';
+import trustwallet from '../../assets/trustwallet.png';
+import cancel from '../../assets/cancel.png';
 const NavigationBar = () => {
+  const [openPopup, setOpenPopup] = useState(false);
+  const handleOpenPopUp = () => {
+    setOpenPopup(true);
+  };
+  const handleClosePopUp = () => {
+    setOpenPopup(false);
+  };
+
   return (
-    <nav className='navbar navbar-expand-lg navbar-dark'>
-      <div className='container'>
-        <h1 className='navbar-brand'>
-          <img src={logo} alt='amigo logo' />
-        </h1>
-        <button
-          className='navbar-toggler'
-          type='button'
-          data-bs-toggle='collapse'
-          data-bs-target='#navbarNav'
-          aria-controls='navbarNav'
-          aria-expanded='false'
-          aria-label='Toggle navigation'
-        >
-          <span className='navbar-toggler-icon'></span>
-        </button>
-        <div className='collapse navbar-collapse' id='navbarNav'>
-          <ul className='navbar-nav ms-auto'>
-            <li className='nav-item ms-md-5'>
-              <Link
-                to='story'
-                spy={true}
-                smooth={true}
-                offset={-100}
-                duration={500}
-                className='nav-link'
-              >
-                The Story
-              </Link>
-            </li>
-            <li className='nav-item ms-md-5'>
-              <Link
-                to='roadmap'
-                spy={true}
-                smooth={true}
-                offset={-50}
-                duration={500}
-                className='nav-link'
-              >
-                Roadmap
-              </Link>
-            </li>
-            <li className='nav-item ms-md-5'>
-              <Link
-                to='team'
-                spy={true}
-                smooth={true}
-                offset={-50}
-                duration={500}
-                className='nav-link'
-              >
-                Team
-              </Link>
-            </li>
-            <li className='nav-item ms-md-5'>
-              <Link
-                to='faqs'
-                spy={true}
-                smooth={true}
-                offset={-50}
-                duration={500}
-                className='nav-link'
-              >
-                FAQ's
-              </Link>
-            </li>
-            <li className='nav-item ms-md-5'>
+    <>
+      {openPopup && (
+        <div className='modal-container'>
+          <div className='custom-modal gradient-container'>
+            <div className='content'>
+              <img
+                src={cancel}
+                alt='cancel'
+                className='cancel'
+                onClick={handleClosePopUp}
+              />
+              <h4>Connect Wallet</h4>
               <button>
-                <div className='dot'></div>Connect Wallet
+                {' '}
+                <img
+                  src={metamask}
+                  alt='meta mask'
+                  className='img-fluid me-3'
+                />{' '}
+                METAMASK
               </button>
-            </li>
-          </ul>
+              <button>
+                <img
+                  src={trustwallet}
+                  alt='meta mask'
+                  className='img-fluid me-3'
+                />
+                TRUSTWALLET
+              </button>
+              <div className='d-flex justify-content-center mt-5'>
+                <a href='https://www.google.co.in/'>Learn how to connect?</a>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </nav>
+      )}
+      <nav className='navbar navbar-expand-lg navbar-dark'>
+        <div className='container'>
+          <h1 className='navbar-brand'>
+            <img src={logo} alt='amigo logo' />
+          </h1>
+          <button
+            className='navbar-toggler'
+            type='button'
+            data-bs-toggle='collapse'
+            data-bs-target='#navbarNav'
+            aria-controls='navbarNav'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
+          >
+            <span className='navbar-toggler-icon'></span>
+          </button>
+          <div className='collapse navbar-collapse' id='navbarNav'>
+            <ul className='navbar-nav ms-auto'>
+              <li className='nav-item ms-md-5'>
+                <Link
+                  to='story'
+                  spy={true}
+                  smooth={true}
+                  offset={-100}
+                  duration={500}
+                  className='nav-link'
+                >
+                  The Story
+                </Link>
+              </li>
+              <li className='nav-item ms-md-5'>
+                <Link
+                  to='roadmap'
+                  spy={true}
+                  smooth={true}
+                  offset={-50}
+                  duration={500}
+                  className='nav-link'
+                >
+                  Roadmap
+                </Link>
+              </li>
+              <li className='nav-item ms-md-5'>
+                <Link
+                  to='team'
+                  spy={true}
+                  smooth={true}
+                  offset={-50}
+                  duration={500}
+                  className='nav-link'
+                >
+                  Team
+                </Link>
+              </li>
+              <li className='nav-item ms-md-5'>
+                <Link
+                  to='faqs'
+                  spy={true}
+                  smooth={true}
+                  offset={-50}
+                  duration={500}
+                  className='nav-link'
+                >
+                  FAQ's
+                </Link>
+              </li>
+              <li className='nav-item ms-md-5'>
+                <button onClick={handleOpenPopUp}>
+                  <div className='dot'></div>Connect Wallet
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
   );
 };
 
