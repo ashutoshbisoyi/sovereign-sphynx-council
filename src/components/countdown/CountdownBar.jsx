123832b853bb3f630a179a206723a7fe69cf453d
ashutoshbisoyi <ashutoshbisoyi205@gmail.com>
2021-10-11 05:05:27 +0000

    timer according to timezone

--- src/components/countdown/CountdownBar.jsx
+++ src/components/countdown/CountdownBar.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import FlipCountdown from '@rumess/react-flip-countdown';
 import './CountdownBar.scss';
+import moment from 'moment';
+import 'moment-timezone';
 const CountdownBar = () => {
+  var tz = moment.tz.guess();
+  const variableTime = moment
+    .utc('2021-10-11 15:30')
+    .tz(tz)
+    .format('YYYY-MM-DD HH:MM:SS');
   return (
     <div className='countdown-bar'>
       <h3>LAUNCHING 10/11 3PM UTC</h3>
@@ -13,7 +20,7 @@ const CountdownBar = () => {
           hourTitle='Hours'
           minuteTitle='Minutes'
           secondTitle='Seconds'
-          endAt={'2021-10-11 20:30:00'}
+          endAt={variableTime}
           titlePosition='bottom'
           endAtZero
           size='medium'
