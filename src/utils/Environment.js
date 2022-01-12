e4ca29f845136cfd169fe732842d21f2bfa2250f
Kashmiri <waleed.shafiq96@gmail.com>
2022-01-13 01:39:09 +0500

    deploy with new contract

--- src/utils/Environment.js
+++ src/utils/Environment.js
@@ -1,5 +1,7 @@
 let Environment = {
-  sphynxAddress: "0xdDE325F362F1DE01ed783C0B07764f30844e4102",
+  // sphynxAddress: "0xdDE325F362F1DE01ed783C0B07764f30844e4102", // old
+  sphynxAddress: "0xcaA15bCA56e951bdd5a302Faa5B1D6d4238a9933",
+
 };
 
 export default Environment;
