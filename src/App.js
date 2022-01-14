fe1fedb5314eb4aef3166745ac2589fef610e1ce
ashutoshbisoyi <ashutoshbisoyi205@gmail.com>
2022-01-14 17:41:30 +0530

    removed countdown

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ import Footer from './components/footer/Footer';
 // import Lunch from './components/lunch/Lunch';
 import Story from './components/story/Story';
 import Rarity from './components/rarity/Rarity';
-import CountdownBar from './components/countdown/CountdownBar';
 import Types from './components/types/Types';
 import useEagerConnect from './hooks/useEagerConnect';
 
@@ -23,7 +22,6 @@ const App = () => {
 
   return (
     <div>
-      <CountdownBar />
       <NavigationBar />
       <GradientAnimation />
       <Hero />
