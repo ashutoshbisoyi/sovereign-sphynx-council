0e619a8759510748cba1e8c62a2aa3ae31e1a901
ashutoshbisoyi <ashutoshbisoyi205@gmail.com>
2022-01-18 12:18:18 +0530

    launch date update

--- src/components/lunch/Lunch.jsx
+++ src/components/lunch/Lunch.jsx
@@ -4,8 +4,8 @@ const Lunch = () => {
   return (
     <section className='container-fluid lunch'>
       <div className='container'>
-        <h2>The Scions Awakening</h2>
-        <p>24th November 2021 | 4:00 P.M. UTC</p>
+        <h2>When is the launch?</h2>
+        <p>The Scion Sphynx SSC Collection launched on January 14 at 1:00 AM UTC</p>
       </div>
     </section>
   );
